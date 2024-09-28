d3d2812fedac877506845a5b0211f6b99b6bb38a
Saurav Kumar <sauravskit749@gmail.com>
2024-09-28 20:58:19 +0530

    intial code

---
+++ infytel-customer/src/main/java/com/infosys/infytel/customer/controller/CustomerController.java
@@ -0,0 +1,51 @@
+package com.infosys.infytel.customer.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.infosys.infytel.customer.dto.CustomerDTO;
+import com.infosys.infytel.customer.dto.LoginDTO;
+import com.infosys.infytel.customer.service.CustomerService;
+
+@RestController
+@CrossOrigin
+public class CustomerController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	CustomerService custService;
+
+	// Create a new customer
+	@RequestMapping(value = "/customers", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void createCustomer(@RequestBody CustomerDTO custDTO) {
+		logger.info("Creation request for customer {}", custDTO);
+		custService.createCustomer(custDTO);
+	}
+
+	// Login
+	@RequestMapping(value = "/login", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean login(@RequestBody LoginDTO loginDTO) {
+		logger.info("Login request for customer {} with password {}", loginDTO.getPhoneNo(),loginDTO.getPassword());
+		return custService.login(loginDTO);
+	}
+
+	// Fetches full profile of a specific customer
+	@RequestMapping(value = "/customers/{phoneNo}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public CustomerDTO getCustomerProfile(@PathVariable Long phoneNo) {
+
+		logger.info("Profile request for customer {}", phoneNo);
+		return custService.getCustomerProfile(phoneNo);
+	}
+
+
+
+}
